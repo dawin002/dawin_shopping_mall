e2f71916217bc52c1a4c33eb7a186e0c2d2facb8
dawin002 <dawin002@naver.com>
2024-03-29 00:10:45 +0900

    create(dawin_shopping_mall): floating action button, tab bar

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -37,21 +37,6 @@ class _FirstScreenState extends State<FirstScreen> {
     });
   }
 
-  // FirebaseStore로 부터 shoppingItems 받아오기 2 (블베님이 뭔가 해보려 했는데 잘 안됨)
-  // Future<void> _updateSoppingItem() async {
-  //   final _tmp = await _firebaseStoreService.fetchShoppingItems();
-  //   setState(() {
-  //     shoppingItems = _tmp.cast<ShoppingItem>();
-  //   });
-  // }
-
-  // final _images = [
-  //   'https://ih0.redbubble.net/image.1322057754.0118/raf,360x360,075,t,fafafa:ca443f4786.jpg',
-  //   'https://mblogthumb-phinf.pstatic.net/MjAyMjEwMjRfMTA4/MDAxNjY2NTQxNTAxODc2.jP5--4FxmfTGv5cFJpj5Xa6-CerWA4plvjuI3x-g2lgg.Qa-im_7FWkAApgjwN9ixQQ69QIY6MvQyqu7SlB9YdqEg.JPEG.gogoa25/IMG_7075.JPG?type=w800',
-  //   'https://mblogthumb-phinf.pstatic.net/MjAyMjEwMjRfMTMg/MDAxNjY2NTQxNTAxMjM4.2qICPuKCeUf6xJmaoY78nyLU1wF4gMVrISKMKWTb6yIg.2sM2m5OfGh1yNQ86hrue2kBCXGGJkaQUQDCNwajUhvcg.JPEG.gogoa25/IMG_7094.JPG?type=w800',
-  //   'https://mblogthumb-phinf.pstatic.net/MjAyMjEwMjRfMTMx/MDAxNjY2NTQxNTAyNTc0.7xNNUaAYuKhHRBRf_rDpoYdVVSM5IyWHUkvJR0lqkiAg.pWW8wOMU-sNwO0EF4gDlaI51Ojm2wbOk0-KHCu8eKQwg.JPEG.gogoa25/IMG_7085.JPG?type=w800',
-  // ];
-
   final _imagesList = [
     Image.network(
         'https://ih0.redbubble.net/image.1322057754.0118/raf,360x360,075,t,fafafa:ca443f4786.jpg'),
@@ -63,103 +48,114 @@ class _FirstScreenState extends State<FirstScreen> {
         'https://mblogthumb-phinf.pstatic.net/MjAyMjEwMjRfMTMx/MDAxNjY2NTQxNTAyNTc0.7xNNUaAYuKhHRBRf_rDpoYdVVSM5IyWHUkvJR0lqkiAg.pWW8wOMU-sNwO0EF4gDlaI51Ojm2wbOk0-KHCu8eKQwg.JPEG.gogoa25/IMG_7085.JPG?type=w800'),
   ];
 
-  final _itemList = [
-    ShoppingItem(id: 'id', name: 'name1', price: 'price', imageSrc: 'imageSrc'),
-    ShoppingItem(id: 'id', name: 'name2', price: 'price', imageSrc: 'imageSrc'),
-    ShoppingItem(id: 'id', name: 'name3', price: 'price', imageSrc: 'imageSrc'),
-    ShoppingItem(id: 'id', name: 'name4', price: 'price', imageSrc: 'imageSrc'),
-    ShoppingItem(id: 'id', name: 'name5', price: 'price', imageSrc: 'imageSrc'),
-    ShoppingItem(id: 'id', name: 'name6', price: 'price', imageSrc: 'imageSrc'),
-    ShoppingItem(id: 'id', name: 'name7', price: 'price', imageSrc: 'imageSrc'),
-    ShoppingItem(id: 'id', name: 'name8', price: 'price', imageSrc: 'imageSrc'),
-  ];
-
+  // build : 화면과 관련된 뭔가를 만들어서 보여주는 친구
   @override
   Widget build(BuildContext context) {
-    // build : 화면과 관련된 뭔가를 만들어서 보여주는 친구
-    // 스크롤 넣을 때는 SingleChildScrollView 로 감싸면 됨
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Text('첫번째 페이지'),
-          CarouselSlider(
-            // 이미지가 필요함!
-            items: _imagesList,
-            options: CarouselOptions(
-              autoPlay: true,
-              enlargeCenterPage: true,
-              viewportFraction: 0.9,
-              aspectRatio: 2.0,
-              initialPage: 0,
-            ),
-            //
-          ),
-          const SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(children: [
-              ElevatedButton(
-                onPressed: null,
-                child: Text('카테고리1'),
-              ),
-              ElevatedButton(
-                onPressed: null,
-                child: Text('카테고리2'),
-              ),
-              ElevatedButton(
-                onPressed: null,
-                child: Text('카테고리3'),
-              ),
-              ElevatedButton(
-                onPressed: null,
-                child: Text('카테고리4'),
-              ),
-              ElevatedButton(
-                onPressed: null,
-                child: Text('카테고리5'),
-              ),
-              ElevatedButton(
-                onPressed: null,
-                child: Text('카테고리6'),
-              ),
-            ]),
-          ),
-          GridView.builder(
-            // 위젯 전체에 걸린 SingleChildScrollView 와 GridView 의 스크롤 충돌 발생하면
-            // 그리드뷰의 스크롤 가능 여부를 지워주면 정상 작동
-            physics: NeverScrollableScrollPhysics(),
-            shrinkWrap: true, // Wrap : 안에 있는 내용물 사이즈만큼 조절해주는 것 Wrap_contents
-            itemCount: _shoppingItems.length,
-            // itemCount: 20,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 10, // 횡 간격
-              mainAxisSpacing: 10, // 종 간격
-            ),
-            itemBuilder: (BuildContext context, int index) {
-              return Column(
-                children: [
-                  Expanded(
-                    child: Image.network(
-                        'https://mblogthumb-phinf.pstatic.net/MjAyMjEwMjRfMTMx/MDAxNjY2NTQxNTAyNTc0.7xNNUaAYuKhHRBRf_rDpoYdVVSM5IyWHUkvJR0lqkiAg.pWW8wOMU-sNwO0EF4gDlaI51Ojm2wbOk0-KHCu8eKQwg.JPEG.gogoa25/IMG_7085.JPG?type=w800'),
-                  ),
-                  Text(_shoppingItems[index].name),
-                  Text(_shoppingItems[index].price),
-                ],
-              );
-            },
-          ),
-          ElevatedButton(
-            child: Text('물건 추가'),
+    // 바디 위젯 안에 앱바(탭바)를 넣기 위해 스캐폴드로 감쌈
+    // 결과적으로 top_screen 의 스캐폴드 안에 first_screen 의 스캐폴드가 들어간 모양
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('홈'),
+        backgroundColor: Colors.redAccent,
+        // 상단 오른쪽에 들어갈 것들 : actions
+        actions: [
+          IconButton(
             onPressed: () {
-              _firebaseStoreService.addShoppingItem(ShoppingItem(
-                  id: "item123",
-                  name: "아버지의 유산",
-                  price: "100",
-                  imageSrc: "imageSrc"));
-              _updateShoppingItem();
+              //
             },
-          ),
+            icon: const Icon(Icons.shopping_cart),
+          )
         ],
+        // AppBar 안의 bottom 자리에 TabBar 가 들어갈 수 있음
+        bottom: TabBar,
+      ),
+      // 앱바 인덱스 값이 바뀌면 바디(카테고리에 따른 상품 리스트)를 변경해주면 된다!
+      body: SingleChildScrollView(
+        // 스크롤 넣을 때는 SingleChildScrollView 로 감싸면 됨
+        // 스캐폴드로 감싸면서 child: -> body: 로 바뀜
+        child: Column(
+          children: [
+            Text('첫번째 페이지'),
+            CarouselSlider(
+              // 이미지가 필요함!
+              items: _imagesList,
+              options: CarouselOptions(
+                autoPlay: true,
+                enlargeCenterPage: true,
+                viewportFraction: 0.9,
+                aspectRatio: 2.0,
+                initialPage: 0,
+              ),
+              //
+            ),
+            const SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(children: [
+                ElevatedButton(
+                  onPressed: null,
+                  child: Text('카테고리1'),
+                ),
+                ElevatedButton(
+                  onPressed: null,
+                  child: Text('카테고리2'),
+                ),
+                ElevatedButton(
+                  onPressed: null,
+                  child: Text('카테고리3'),
+                ),
+                ElevatedButton(
+                  onPressed: null,
+                  child: Text('카테고리4'),
+                ),
+                ElevatedButton(
+                  onPressed: null,
+                  child: Text('카테고리5'),
+                ),
+                ElevatedButton(
+                  onPressed: null,
+                  child: Text('카테고리6'),
+                ),
+              ]),
+            ),
+            GridView.builder(
+              // 위젯 전체에 걸린 SingleChildScrollView 와 GridView 의 스크롤 충돌 발생하면
+              // 그리드뷰의 스크롤 가능 여부를 지워주면 정상 작동
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              // Wrap : 안에 있는 내용물 사이즈만큼 조절해주는 것 Wrap_contents
+              itemCount: _shoppingItems.length,
+              // itemCount: 20,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10, // 횡 간격
+                mainAxisSpacing: 10, // 종 간격
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                return Column(
+                  children: [
+                    Expanded(
+                      child: Image.network(
+                          'https://mblogthumb-phinf.pstatic.net/MjAyMjEwMjRfMTMx/MDAxNjY2NTQxNTAyNTc0.7xNNUaAYuKhHRBRf_rDpoYdVVSM5IyWHUkvJR0lqkiAg.pWW8wOMU-sNwO0EF4gDlaI51Ojm2wbOk0-KHCu8eKQwg.JPEG.gogoa25/IMG_7085.JPG?type=w800'),
+                    ),
+                    Text(_shoppingItems[index].name),
+                    Text(_shoppingItems[index].price),
+                  ],
+                );
+              },
+            ),
+            ElevatedButton(
+              child: Text('물건 추가'),
+              onPressed: () {
+                _firebaseStoreService.addShoppingItem(ShoppingItem(
+                    id: "item123",
+                    name: "아버지의 유산",
+                    price: "100",
+                    imageSrc: "imageSrc"));
+                _updateShoppingItem();
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/top_screen.dart
+++ lib/top_screen.dart
@@ -3,27 +3,29 @@ import 'package:flutter/material.dart';
 
 int _bottomNavigationBarIndex = 0;
 
-AppBar _buildAppBar() {
-  // 조건문 추가하자!
-  switch (_bottomNavigationBarIndex) {
-    case 0:
-      return AppBar(
-        title: Text('홈'),
-        backgroundColor: Colors.redAccent,
-      );
-    case 1:
-      return AppBar(
-        title: Text('상품'),
-        backgroundColor: Colors.greenAccent,
-      );
-    default:
-      return AppBar(
-        title: Text('home'),
-        backgroundColor: Colors.cyanAccent,
-      );
-  }
-}
+// 앱바 대신 탭바로 바꾸려고 함
+// AppBar _buildAppBar() {
+//   // 조건문 추가하자!
+//   switch (_bottomNavigationBarIndex) {
+//     case 0:
+//       return AppBar(
+//         title: Text('홈'),
+//         backgroundColor: Colors.redAccent,
+//       );
+//     case 1:
+//       return AppBar(
+//         title: Text('상품'),
+//         backgroundColor: Colors.greenAccent,
+//       );
+//     default:
+//       return AppBar(
+//         title: Text('home'),
+//         backgroundColor: Colors.cyanAccent,
+//       );
+//   }
+// }
 
+// 빌드 바디 : 선택한 메뉴에 따라 바디 내용을 다르게 보여주는 것
 Widget _buildBody() {
   switch (_bottomNavigationBarIndex) {
     case 0:
@@ -32,6 +34,26 @@ Widget _buildBody() {
       return const Center();
     default:
       return Container();
+  }
+}
+
+// 빌드 플로팅 액션 버튼 함수
+Widget? _buildFloatingActionButton() {
+  // 선택한 바텀 메뉴에 따라 플로팅 액션 버튼을 보여줄지 말지 결정
+  // null 을 반환할 수도 있기 때문에 반환 타입 Widget? (물음표 붙임)
+  switch (_bottomNavigationBarIndex) {
+    case 0:
+      // 플로팅 액션 버튼 반환
+      return FloatingActionButton(
+        onPressed: () {
+          print('Floating Action Button Clicked');
+        },
+        backgroundColor: Colors.blue,
+        child: const Icon(Icons.add),
+      );
+    default:
+      // 널 반환 (플로팅 액션 버튼 가림)
+      return null;
   }
 }
 
@@ -46,7 +68,8 @@ class _TopScreenState extends State<TopScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _buildAppBar(), // 앱바가 들어갈 자리
+      // 앱바를 top_screen 이 아닌 first_screen 에서 관리하기 위해 지움
+      // appBar: _buildAppBar(), // 앱바가 들어갈 자리
       body: _buildBody(), // 위젯이 들어갈 자리
       bottomNavigationBar: BottomNavigationBar(
         // 선택된 값을 어떻게 가져올까 : currentIndex, onTap
@@ -72,6 +95,8 @@ class _TopScreenState extends State<TopScreen> {
           ),
         ],
       ),
+      // 고정된 위치에 떠있는 버튼, 앱바/바디/바텀내비게이션바 와 같은 레벨
+      floatingActionButton: _buildFloatingActionButton(),
     );
   }
 }
